921a918b0aeb05a87998252c5051296b02a9ec97
dexter <soul9862@gmail.com>
2021-06-22 22:49:53 +0900

    git init

---
+++ src/main/java/com/api/ServerApplication.java
@@ -0,0 +1,25 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, // spring boot 에서 default 사용하는거 뺌
+		MongoDataAutoConfiguration.class,  // 현재 하이버네이트 방언이 Mysql 로 되어있음.
+		MultipartAutoConfiguration.class}) // Tomcat 의 Multipart Library 사용하기 때문에 spring boot 의 Multipart 기능을 exclude 함.
+public class ServerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServerApplication.class);
+	}
+
+}
---
+++ src/main/java/com/api/domain/Member.java
@@ -0,0 +1,45 @@
+package com.api.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@DynamicInsert
+@Data
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity implements Serializable  {
+
+    private static final long serialVersionUID = -6430942442101448953L;
+
+    @Column(unique = true, length = 50)
+    private String account;
+
+    @Column(length = 100)
+    private String password;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 1000)
+    private String token;
+
+    @Builder
+    public Member(String account, String password, String name, String token) {
+        this.account = account;
+        this.password = password;
+        this.name = name;
+        this.token = token;
+
+    }
+
+}
